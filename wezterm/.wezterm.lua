628115f1ef4c740d71f68aacab61b36168761d1f
AndrMaulana <andryymaulana2000@gmail.com>
2025-02-27 14:43:31 +0700

    refac config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,11 +1,1 @@
-local wezterm = require("wezterm")
-local config = wezterm.config_builder()
 
-config = {
-	default_cursor_style = "Steadybar",
-	warn_about_missing_glyphs = false,
-	automatically_reload_config = true,
-	window_close_confirmation = "NeverPrompt",
-}
-
-return config
